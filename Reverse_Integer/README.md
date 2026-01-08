2a7534e7082e8114b94202fd739637ac9e15cf52
Kunal-Kamod25 <kunalkamod3@gmail.com>
2026-01-08 18:12:30 +0530

    Add Reverse integer solution

---
+++ Reverse_Integer/README.md
@@ -0,0 +1,32 @@
+\# Reverse Integer
+
+
+
+LeetCode Problem #7 – Reverse Integer
+
+
+
+\## Example
+
+Input: 123  
+
+Output: 321
+
+
+
+\## Approach
+
+\- Reverse digits using modulo
+
+\- Handle overflow safely
+
+
+
+\## Complexity
+
+Time: O(n)  
+
+Space: O(1)
+
+
+
---
+++ Reverse_Integer/reverse_integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int ans=0;
+        int MAX=INT_MAX/10 , MIN=INT_MIN/10;
+        while (x!=0)
+        {
+            if(MAX < ans || MIN > ans)
+                return 0;
+            ans = ans * 10 + (x % 10);
+            x=x/10;
+        }
+        return ans;
+    }
+};
