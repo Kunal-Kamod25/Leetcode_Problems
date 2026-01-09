a7b80f262d500515a7baded916c4c8093edce6b5
Kunal-Kamod25 <kunalkamod3@gmail.com>
2026-01-09 21:14:13 +0530

    Add Shuffle the array

---
+++ Leetcode_Solutions/1470_Shuffle_the_array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> ans (2*n);
+        int ans_i=0;
+        for(int i = 0 ; i < n ; i++)
+        {
+            ans[ans_i] = nums[i];
+            ans[ans_i + 1] = nums[i + n];
+            ans_i = ans_i + 2;
+        }
+        return ans;
+    }
+};
