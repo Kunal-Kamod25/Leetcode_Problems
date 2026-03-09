0cfd62b87a78ec56b19622828e9d0df72b9f4060
Kunal-Kamod25 <kunalkamod3@gmail.com>
2026-03-09 23:19:32 +0530

    150 No problem solved

---
+++ Leetcode_Solutions/150_Evaluate_Reverse_Polish_Notation.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int>stk;
+        for(const string& t: tokens){
+            if(t =="+" || t =="-" || t =="*" || t =="/") {
+                int rtg = stk.top() ; stk.pop();
+                int lft = stk.top() ; stk.pop();
+                int ans = 0;
+
+                if(t=="+") ans = lft + rtg;
+                else if(t=="-") ans = lft - rtg;
+                else if(t=="*") ans = lft * rtg;
+                else if(t=="/") ans = lft / rtg;
+
+                stk.push(ans);
+            }
+            else{
+                stk.push(stoi(t));
+            }
+        }
+        return stk.top();
+        
+    }
+};
