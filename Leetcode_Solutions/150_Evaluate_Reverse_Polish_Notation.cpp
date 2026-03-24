f9bc6b061fa929120d5339a0d1b1d51a0cdac39d
Kunal-Kamod25 <kunalkamod3@gmail.com>
2026-03-24 13:26:41 +0530

    Added 2390 + updated 150

--- Leetcode_Solutions/150_Evaluate_Reverse_Polish_Notation.cpp
+++ Leetcode_Solutions/150_Evaluate_Reverse_Polish_Notation.cpp
@@ -1,25 +1,25 @@
-class Solution {
-public:
-    int evalRPN(vector<string>& tokens) {
-        stack<int>stk;
-        for(const string& t: tokens){
-            if(t =="+" || t =="-" || t =="*" || t =="/") {
-                int rtg = stk.top() ; stk.pop();
-                int lft = stk.top() ; stk.pop();
-                int ans = 0;
+    class Solution {
+    public:
+        int evalRPN(vector<string>& tokens) {
+            stack<int>stk;
+            for(const string& t: tokens){
+                if(t =="+" || t =="-" || t =="*" || t =="/") {
+                    int rtg = stk.top() ; stk.pop();
+                    int lft = stk.top() ; stk.pop();
+                    int ans = 0;
 
-                if(t=="+") ans = lft + rtg;
-                else if(t=="-") ans = lft - rtg;
-                else if(t=="*") ans = lft * rtg;
-                else if(t=="/") ans = lft / rtg;
+                    if(t=="+") ans = lft + rtg;
+                    else if(t=="-") ans = lft - rtg;
+                    else if(t=="*") ans = lft * rtg;
+                    else if(t=="/") ans = lft / rtg;
 
-                stk.push(ans);
+                    stk.push(ans);
+                }
+                else{
+                    stk.push(stoi(t));
+                }
             }
-            else{
-                stk.push(stoi(t));
-            }
+            return stk.top();
+            
         }
-        return stk.top();
-        
-    }
-};
+    };
