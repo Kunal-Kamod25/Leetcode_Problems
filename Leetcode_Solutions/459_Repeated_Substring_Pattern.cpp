6fa2cb3d09475965f56dfed2c586c7933c1e42ca
Kunal-Kamod25 <kunalkamod3@gmail.com>
2026-06-17 19:01:00 +0530

    Leetcode Problem Number 459

---
+++ Leetcode_Solutions/459_Repeated_Substring_Pattern.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        int sz = s.size();
+        for(int i = 0; i < sz/2; i++) {
+            int len = i + 1;
+            if(sz % len != 0)
+                continue;
+
+            int rep = sz/len;
+            string s1 = s.substr(0 , len);
+            string s2;
+
+            for(int j = 0; j < rep; j++) {
+                s2 += s1;
+                if(s2 == s)
+                    return true;
+            }
+            
+        }
+        return false;
+    }
+};
